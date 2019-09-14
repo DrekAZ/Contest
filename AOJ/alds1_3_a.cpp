b4c14dc494f65de45e712650278ccdc91f961ba8
A <kassuruv2@gmail.com>
2019-09-14 21:22:37 +0900

    9/14

--- AOJ/alds1_3_a.cpp
+++ AOJ/alds1_3_a.cpp
@@ -1,9 +1,8 @@
 #include <cstdio>
 #include <stack>
 #include <cstdlib>
-#include <iostream>
 using namespace std;
-
+/// return version
 /*int f(stack<char> s){
     char str = s.top();
     s.pop();
@@ -14,7 +13,9 @@ using namespace std;
     return str - '0';
 }*/
 
-
+/*
+11 11 + == 22 111 111 + == 11 ?????? It's mistery so I used t3
+*/
 main(){
     char str[101];
     int t1, t2;
@@ -27,27 +28,23 @@ main(){
             s.pop();
             int t3 = t2+t1;
             s.push(t3);
-            printf("%d\n", s.top());
         } else if(str[0] == '-'){
             t1 = s.top();
             s.pop();
             t2 = s.top();
             s.pop();
-            s.push( t2 - t1 );
-            printf("%d - %d\n",t2, t1);
+            int t3 = t2-t1;
+            s.push( t3 );
         } else if(str[0] == '*'){
             t1 = s.top();
             s.pop();
             t2 = s.top();
             s.pop();
-            s.push( t2 * t1 );
-            printf("%d * %d\n",t2, t1);
+            int t3 = t2*t1;
+            s.push( t3 );
         } else {
             s.push( atoi(str) );
-            printf("%d\n", atoi(str));
         }
     }
-    t1 = s.top();
-    printf("%d\n", t1);
-    cout << s.size() << endl;
+    printf("%d\n", s.top());
 }
